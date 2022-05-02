e4cd2217f94250b4f2c69906d9b52c96e7197ac9
ggreenn <hers573@hsd.k12.or.us>
2022-05-02 14:56:46 -0700

    Edpuzzle part 1 and 2

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // Test Scripts
+
+//Create a paragraph tag
+var paragraph = document.createElement('P');
+
+//Add text to paragraph
+paragraph.textContent = 'The DOM is the bomb!';
+
+//Add text to body
+document.body.appendChild(paragraph);
+
+
+
+//Part two of DOM Project
+
+//Create a DIV Element
+var div = document.createElement('DIV');
+
+//Set Height of Element
+div.style.height = '100vh';
+
+//Append Element to DOM
+document.body.appendChild(div);
+div.addEventListener('mousemove', function(event) {
+    console.log(event);
+    var x = event.clientX;
+    var y = event.clientY;
+    div.textContent = x + ', ' + y;
+    div.style.backgroundColor = 'rbg(' + x + '. ' +y + ',100)'
+});
